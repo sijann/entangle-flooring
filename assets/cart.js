c2bbd50ac3f1007a83c01cf75b1575523a5e8bfd
Sijan Paudel <sijannpaudel.com>
2023-08-17 10:55:18 +0545

    Cart AJAX updated.(both drawer and page)

--- assets/cart.js
+++ assets/cart.js
@@ -1,51 +1,3 @@
-
-function formatMoney(cents, format) {
-  if (typeof cents == 'string') { cents = cents.replace('.', ''); }
-  var value = '';
-  var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
-  var formatString = (format || this.money_format);
-
-  function defaultOption(opt, def) {
-    return (typeof opt == 'undefined' ? def : opt);
-  }
-
-  function formatWithDelimiters(number, precision, thousands, decimal) {
-    precision = defaultOption(precision, 2);
-    thousands = defaultOption(thousands, ',');
-    decimal = defaultOption(decimal, '.');
-
-    if (isNaN(number) || number == null) { return 0; }
-
-    number = (number / 100.0).toFixed(precision);
-
-    var parts = number.split('.'),
-      dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
-      cents = parts[1] ? (decimal + parts[1]) : '';
-
-    return dollars + cents;
-  }
-
-  switch (formatString.match(placeholderRegex)[1]) {
-    case 'amount':
-      value = formatWithDelimiters(cents, 2);
-      break;
-    case 'amount_no_decimals':
-      value = formatWithDelimiters(cents, 0);
-      break;
-    case 'amount_with_comma_separator':
-      value = formatWithDelimiters(cents, 2, '.', ',');
-      break;
-    case 'amount_no_decimals_with_comma_separator':
-      value = formatWithDelimiters(cents, 0, '.', ',');
-      break;
-  }
-
-  return formatString.replace(placeholderRegex, value);
-};
-
-
-
-
 // AddtoCart 
 
 const addToCartForms = document.querySelectorAll('form[action="/cart/add"]');
@@ -82,53 +34,16 @@ addToCartForms.forEach((form) => {
 
 
 
-// Upadate PlusMinus Buttons
 
-const removeFromCartButtons = document.querySelectorAll(".remove-from-cart")
-removeFromCartButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault()
-    const key = button.closest(".cart-item").getAttribute("data-key");
-    changeItemQuantity(key, 0);
-  })
-})
-const cartQuantitySelectorButtons = document.querySelectorAll(".cart-quantity-selector button")
-const cartInputFields = document.querySelectorAll(".cart-quantity-selector input")
 
-cartInputFields.forEach((input) => {
-  input.addEventListener("change", () => {
-    let value = Number(input.value)
-    const key = input.closest(".cart-item").getAttribute("data-key")
-    changeItemQuantity(key, value)
-  })
-})
-
-cartQuantitySelectorButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    event.preventDefault()
-    const input = button.parentElement.querySelector("input")
-    let value = Number(input.value)
-    const isPlus = button.classList.contains("plus")
-    const key = button.closest(".cart-item").getAttribute("data-key")
-    if (isPlus) {
-      input.value = value + 1;
-      changeItemQuantity(key, value + 1)
-    } else if (value > 1) {
-      input.value = value - 1;
-      changeItemQuantity(key, value - 1)
-    }
-  })
-})
-
-async function changeItemQuantity(key, quantity) {
-
-  document.querySelector(`[data-key="${key}"] .plus`).disabled = true;
-  document.querySelector(`[data-key="${key}"] .minus`).disabled = true;
-  document.querySelector(`[data-key="${key}"] input`).disabled = true;
-
-  document.querySelector(`[data-key="${key}"] .line-item-price`).classList.add("hidden");
-  document.querySelector(`[data-key="${key}"] .spinner--overlay`).classList.remove("hidden");
-  const response = await fetch("/cart/change.js", {
+async function changeItemQuantity(key, quantity, section) {
+  let target = document.getElementById(section)
+  target.querySelector(`[data-key="${key}"] .plus`).disabled = true;
+  target.querySelector(`[data-key="${key}"] .minus`).disabled = true;
+  target.querySelector(`[data-key="${key}"] input`).disabled = true;
+  target.querySelector(`[data-key="${key}"] .line-item-price`).classList.add("hidden");
+  target.querySelector(`[data-key="${key}"] .spinner--overlay`).classList.remove("hidden");
+  fetch("/cart/change.js", {
     method: "post",
     headers: {
       "Content-Type": "application/json"
@@ -137,56 +52,32 @@ async function changeItemQuantity(key, quantity) {
       id: key,
       quantity,
     }),
-  });
+  }).then(() => {
+
+    fetch(`/cart?section_id=${section}`).then((response) => response.text())
+      .then((responseText) => {
+        const html = new DOMParser().parseFromString(responseText, 'text/html');
+        if(section == 'cart-drawer'){
+          let elem = document.getElementById("cart-drawer") 
+          elem.innerHTML = html.getElementById("cart-drawer").innerHTML
+        }else{
+          let elem = document.getElementById("en-cart") 
+          elem.innerHTML = html.getElementById("en-cart").innerHTML
+        }
+      }).finally(()=>{
+      });
 
 
-  const data = await response.json();
-
-  const format = document.querySelector('[data-money-format]').getAttribute('data-money-format')
-  const totalDiscount = formatMoney(data.total_discount, format);
-  const totalPrice = formatMoney(data.total_price, format);
-  console.log(data)
-
-  let itemPrice = 0
-  let finalLinePrice;
-  if (quantity !== 0) {
-    const item = data.items.find(item => item.key === key)
-    itemPrice = formatMoney(item.final_line_price, format);
-    finalLinePrice = formatMoney(item.final_price, format);
-  }
+  })
+    
 
 
-  const itemsInCart = data.item_count;
-
-
-  document.querySelector(".cart-count").textContent = itemsInCart;
-
-  // document.querySelector("#total-discount").textContent = totalDiscount;
-
-  document.querySelector("#total-price").textContent = totalPrice;
-
-  // Update final line price
-  document.querySelector(`[data-key="${key}"] .final-line-price`).textContent = finalLinePrice;
-
-  // Update final Price
-  document.querySelector(`[data-key="${key}"] .line-item-price`).textContent = itemPrice;
+    
 
 
 
-  document.querySelector(`[data-key="${key}"] .line-item-price`).classList.remove("hidden");
-  document.querySelector(`[data-key="${key}"] .spinner--overlay`).classList.add("hidden");
-  document.querySelector(`[data-key="${key}"] .plus`).disabled = false;
-  document.querySelector(`[data-key="${key}"] .minus`).disabled = false;
-  document.querySelector(`[data-key="${key}"] input`).disabled = false;
 
-  if (quantity === 0) {
-    document.querySelector(`[data-key="${key}"]`).remove();
-  }
-
-  if (itemsInCart === 0) {
-    document.querySelector('.cart-has-item').classList.add("hidden")
-    document.querySelector('.cart-no-item').classList.remove("hidden")
-  }
+  
 
 }
 
@@ -196,6 +87,9 @@ class QuantityButton extends HTMLElement {
   constructor() {
     super();
     this.addEventListener("click", this.onClick.bind(this));
+    this.input = this.parentElement.querySelector("input");
+    this.input.addEventListener("change", this.onClick.bind(this), this.getAttribute("data-section")) 
+
   }
 
   onClick() {
@@ -206,10 +100,10 @@ class QuantityButton extends HTMLElement {
 
     if (isPlus) {
       input.value = value + 1;
-      changeItemQuantity(key, value + 1);
+      changeItemQuantity(key, value + 1, this.getAttribute("data-section"));
     } else if (value > 1) {
       input.value = value - 1;
-      changeItemQuantity(key, value - 1);
+      changeItemQuantity(key, value - 1, this.getAttribute("data-section"));
     }
   }
 }
@@ -224,7 +118,7 @@ class RemoveFromCartButton extends HTMLElement {
   onClick(event) {
     event.preventDefault();
     const key = this.closest(".cart-item").getAttribute("data-key");
-    changeItemQuantity(key, 0);
+    changeItemQuantity(key, 0, this.getAttribute("data-section"));
   }
 }
 
